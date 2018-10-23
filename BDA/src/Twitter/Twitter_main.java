19d32a3854012fc261fedc336ddda9b80b90399b
Hugofc <hmcfc@iscte-iul.pt>
2018-10-23 23:50:52 +0100

    Twitter Tokens

--- BDA/src/Twitter/Twitter_main.java
+++ BDA/src/Twitter/Twitter_main.java
@@ -1,7 +1,38 @@
 package Twitter;
 
-public class Twitter_main {
+import java.util.List;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public final class Twitter_main  {
 	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
+		// http://twitter4j.org
+		// http://twitter4j.org/en/code-examples.html
+		// https://www.youtube.com/watch?v=uYPmkzMpnxw
+        try {
+        	ConfigurationBuilder cb = new ConfigurationBuilder();
+        	cb.setDebugEnabled(true)
+        	  .setOAuthConsumerKey("pRXsTFA4vsYjkjYIphXveKKgV")
+        	  .setOAuthConsumerSecret("CfNV7ZgEwN2hhysP8kAGmy6hWMF7mypqX6CPtRwDy7rQ63Z5rU")
+        	  .setOAuthAccessToken("1053263399889653761-Eu236nomWzitNbXoEcKsOvQVCOchZw")
+        	  .setOAuthAccessTokenSecret("jKSOccpi450YERoIGJeP9qPQLRJbGTrEZtKoG4IlRDuip");
+        	TwitterFactory tf = new TwitterFactory(cb.build());
+        	Twitter twitter = tf.getInstance();        		
+            List<Status> statuses = twitter.getHomeTimeline();
+            System.out.println("------------------------\n Showing home timeline \n------------------------");
+    		int counter=0;
+    		int counterTotal = 0;
+            for (Status status : statuses) {
+				if (status.getUser().getName() != null) {
+					System.out.println(status.getUser().getName() + ":" + status.getText());
+					counter++;
+				}
+				counterTotal++;
+            }
+    		System.out.println("-------------\nNº of Results: " + counter+"/"+counterTotal);
+        } catch (Exception e) { System.out.println(e.getMessage()); }
+     }
 }
+    
