4d699935166108caf46603289d9a3b4cbf02f511
lmmfs-iscteiulpt <lmmfs@iscte-iul.pt>
2018-10-24 21:36:07 +0100

    JUnit test for gamil connection

---
+++ BDA/src/GmailTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class GmailTest {
+
+	@Test
+	void connectionTest() throws IOException {
+		RetrieveEmailsUsingIMAP tmu = new RetrieveEmailsUsingIMAP();
+		String connect = tmu.getConnectedStatus("imap", "imap.gmail.com", "993"
+				, "Trabalhosiscte12@gmail.com", "CrokaNation12");
+		assertEquals("Connected_to_IMAP", connect );
+	}
+	
+	@Test
+	void failConnectionTest() throws IOException {
+		RetrieveEmailsUsingIMAP tmu = new RetrieveEmailsUsingIMAP();
+		String connect = tmu.getConnectedStatus("imap", "imap.gmail.com", "993"
+				, "Trabalhosiscte12@gmail.com", "CrokaNation1000");
+		assertEquals("Could not connect to the message store", connect );
+	}
+	
+	@Test
+	void htmlRemvTest() {
+		RetrieveEmailsUsingIMAP tmu = new RetrieveEmailsUsingIMAP();
+		String htmlFree = tmu.htmlRemove("<div>olá Eu, sou Eu!!!");
+		assertEquals(" olá Eu, sou Eu!!!", htmlFree);
+	}
+
+}
