487899e01c8c2620c8f0f52475cd9fcccfeb30da
Hugofc <hmcfc@iscte-iul.pt>
2018-10-24 20:52:16 +0100

    Text divided into Classes and functions

---
+++ BDA/src/Twitter/Twitter_Class.java
@@ -0,0 +1,63 @@
+package Twitter;
+
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class Twitter_Class {
+	ConfigurationBuilder cbc;
+	/**
+	 * Initiates a Twiiter_Class object
+	 * @param ConfigurationBuilder
+	 */
+	public Twitter_Class(ConfigurationBuilder cb) {
+		this.cbc=cb;
+		
+	}
+	/**
+	 * Initiates a Twitter account with the given tokens
+	 * 
+	 */
+	public void sign_in(){
+		cbc.setDebugEnabled(true)
+  	  	.setOAuthConsumerKey("pRXsTFA4vsYjkjYIphXveKKgV")
+  	  	.setOAuthConsumerSecret("CfNV7ZgEwN2hhysP8kAGmy6hWMF7mypqX6CPtRwDy7rQ63Z5rU")
+  	  	.setOAuthAccessToken("1053263399889653761-Eu236nomWzitNbXoEcKsOvQVCOchZw")
+  	  	.setOAuthAccessTokenSecret("jKSOccpi450YERoIGJeP9qPQLRJbGTrEZtKoG4IlRDuip");
+	}
+	/**
+	 * Prints the first 20 Tweets from the logged user
+	 * 
+	 */
+	public void printTweets() {
+		TwitterFactory tf = new TwitterFactory(cbc.build());
+    	Twitter twitter = tf.getInstance();        		
+        List<Status> statuses;
+		try {
+			statuses = twitter.getHomeTimeline();
+			System.out.println("------------------------\n Showing home timeline \n------------------------");
+			int counter=0;
+			int counterTotal = 0;
+			for (Status status : statuses) {
+				if (status.getUser().getName() != null) {
+					System.out.println(" ");
+					System.out.println(status.getUser().getName() + ":" + status.getText() + " - " + status.getCreatedAt());
+					System.out.println(" ");
+					System.out.println("-------------------------------------------------------------------");
+					counter++;
+				}
+				counterTotal++;
+			}
+			System.out.println("-------------\nNº of Results: " + counter+"/"+counterTotal);
+			} catch (TwitterException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}
+	
+}
--- BDA/src/Twitter/Twitter_main.java
+++ BDA/src/Twitter/Twitter_main.java
@@ -8,30 +8,12 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public final class Twitter_main  {
 	public static void main(String[] args) {
-		// http://twitter4j.org
-		// http://twitter4j.org/en/code-examples.html
 		// https://www.youtube.com/watch?v=uYPmkzMpnxw
         try {
         	ConfigurationBuilder cb = new ConfigurationBuilder();
-        	cb.setDebugEnabled(true)
-        	  .setOAuthConsumerKey("pRXsTFA4vsYjkjYIphXveKKgV")
-        	  .setOAuthConsumerSecret("CfNV7ZgEwN2hhysP8kAGmy6hWMF7mypqX6CPtRwDy7rQ63Z5rU")
-        	  .setOAuthAccessToken("1053263399889653761-Eu236nomWzitNbXoEcKsOvQVCOchZw")
-        	  .setOAuthAccessTokenSecret("jKSOccpi450YERoIGJeP9qPQLRJbGTrEZtKoG4IlRDuip");
-        	TwitterFactory tf = new TwitterFactory(cb.build());
-        	Twitter twitter = tf.getInstance();        		
-            List<Status> statuses = twitter.getHomeTimeline();
-            System.out.println("------------------------\n Showing home timeline \n------------------------");
-    		int counter=0;
-    		int counterTotal = 0;
-            for (Status status : statuses) {
-				if (status.getUser().getName() != null) {
-					System.out.println(status.getUser().getName() + ":" + status.getText());
-					counter++;
-				}
-				counterTotal++;
-            }
-    		System.out.println("-------------\nNº of Results: " + counter+"/"+counterTotal);
+        	Twitter_Class tw = new Twitter_Class(cb);
+        	tw.sign_in();
+        	tw.printTweets();
         } catch (Exception e) { System.out.println(e.getMessage()); }
      }
 }
