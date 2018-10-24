62b5b5615b49b21872a00eefd6dd0049f8f98a18
lmmfs-iscteiulpt <lmmfs@iscte-iul.pt>
2018-10-24 21:26:56 +0100

    JUnit test

--- BDA/src/RetrieveEmailsUsingIMAP.java
+++ BDA/src/RetrieveEmailsUsingIMAP.java
@@ -38,8 +38,10 @@ public class RetrieveEmailsUsingIMAP {
 		properties.put(String.format("mail.%s.port", protocol), port);
 		properties.setProperty(String.format("mail.%s.socketFactory.class", protocol),
 				"javax.net.ssl.SSLSocketFactory");
-		properties.setProperty(String.format("mail.%s.socketFactory.fallback", protocol), "false");
-		properties.setProperty(String.format("mail.%s.socketFactory.port", protocol), String.valueOf(port));
+		properties.setProperty(String.format("mail.%s.socketFactory.fallback",
+				protocol), "false");
+		properties.setProperty(String.format("mail.%s.socketFactory.port",
+				protocol), String.valueOf(port));
 		return properties;
 	}
 
