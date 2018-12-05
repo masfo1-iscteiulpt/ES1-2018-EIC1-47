d4747237a56a5ca5805bac94ac4d816515b4c230
lmmfs-iscteiulpt <lmmfs@iscte-iul.pt>
2018-12-05 17:03:03 +0000

    changes to junit tests

--- BDA/src/gmail/SendMailTest.java
+++ BDA/src/gmail/SendMailTest.java
@@ -8,7 +8,8 @@ public class SendMailTest {
 
 	@Test
 	public void test() {
-		MailSender sender = new MailSender("Trabalhosiscte12@mail.com");
+		MailSender sender = new MailSender();
+		sender.setDestination("Trabalhosiscte12@mail.com");
 		boolean test = sender.sendMessage("Testing SMTP-SSL", "This is a test");
 		assertTrue(test);
 
