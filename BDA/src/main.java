f015bc468ef7a541d6f6da45626d1bdbef876daa
dffsa1 <dffsa1@iscte-iul.pt>
2018-12-06 19:08:26 +0000

    import managing

--- BDA/src/main.java
+++ BDA/src/main.java
@@ -1,17 +1,14 @@
 import java.awt.EventQueue;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.ArrayList;
-
 import javax.mail.NoSuchProviderException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -28,13 +25,10 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
 import twitter4j.conf.ConfigurationBuilder;
 import facebook.Facebook;
 import gmail.RetrieveEmailsUsingIMAP;
